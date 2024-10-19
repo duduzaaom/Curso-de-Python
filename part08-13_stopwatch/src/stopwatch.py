ac1fc03bfef78e8b1ca44ed0cde0b1c3ea132804
Pedro Paulo <gsepi.ufba@gmail.com>
2024-10-19 10:26:25 -0300

    Apagado o arquivo de teste

--- part08-13_stopwatch/src/stopwatch.py
+++ part08-13_stopwatch/src/stopwatch.py
@@ -1,7 +1,5 @@
-# Write your solution here:
 class Stopwatch:
     def __init__(self):
         self.seconds = 0
         self.minutes = 0
 
-print("Isso é mais um teste")
--- part08-13_stopwatch/src/teste.py
+++
@@ -1,2 +0,0 @@
-def estou_testando():
-    pass
