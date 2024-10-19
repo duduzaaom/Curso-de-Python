2a1106e85abe539eb5eac45c53c2fe0a96145a22
Pedro Paulo <gsepi.ufba@gmail.com>
2024-10-19 10:57:26 -0300

    stopwatch resolvido

--- part08-13_stopwatch/src/stopwatch.py
+++ part08-13_stopwatch/src/stopwatch.py
@@ -3,3 +3,34 @@ class Stopwatch:
         self.seconds = 0
         self.minutes = 0
 
+    def tick(self):
+        self.seconds += 1
+        
+        if self.seconds == 60:
+            self.seconds = 0
+            self.minutes += 1
+
+            if self.minutes == 60:
+                self.minutes = 0
+
+    def __str__(self) -> str:
+        line = ''
+        if self.minutes < 10:
+            line += f'0{self.minutes}:'
+        else:
+            line += f'{self.minutes}:'
+
+        if self.seconds < 10:
+            line += f'0{self.seconds}'
+        else:
+            line += f'{self.seconds}'
+
+        return line
+    
+
+if __name__ == "__main__":
+    watch = Stopwatch()
+    for i in range(3600):
+        print(watch)
+        watch.tick()
+
