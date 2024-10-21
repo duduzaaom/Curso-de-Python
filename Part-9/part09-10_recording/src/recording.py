dff573f13768da7dd7a2d03f4b3cf42fde1b6520
Pedro Paulo <gsepi.ufba@gmail.com>
2024-10-21 14:28:05 -0300

    resolvido recording

--- Part-9/part09-10_recording/src/recording.py
+++ Part-9/part09-10_recording/src/recording.py
@@ -1,1 +1,24 @@
-# WRITE YOUR SOLUTION HERE:
+class Recording:
+    def __init__(self, length: int) -> None:
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("The length of the recording should be a positive number.") 
+
+    @property
+    def length(self):
+        return self.__length
+    
+    @length.setter
+    def length(self, length):
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("The length of the recording should be a positive number.")
+        
+
+if __name__ == "__main__":
+    the_wall = Recording(43)
+    print(the_wall.length)
+    the_wall.length = 44
+    print(the_wall.length)
