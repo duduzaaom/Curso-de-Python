ecf4927c6f1f48a8b69aa13687c53e27c2a147d9
Pedro Paulo <gsepi.ufba@gmail.com>
2024-10-21 14:10:31 -0300

    resolvido car

--- Part-9/part09-09_car/src/car.py
+++ Part-9/part09-09_car/src/car.py
@@ -1,5 +1,34 @@
-def HelloWorld(str: str):
-    print(str)
+class Car:
+    def __init__(self) -> None:
+        self.__petrol = 0
+        self.__odometer = 0
+    
+    def fill_up(self):
+        self.__petrol = 60
+    
+    def drive(self, distance: int):
+        if distance <= self.__petrol:
+            self.__odometer += distance
+            self.__petrol -= distance
+        else:
+            self.__odometer += self.__petrol
+            self.__petrol = 0
 
+    def __str__(self):
+        return f"Car: odometer reading {self.__odometer} km, petrol remaining {self.__petrol} litres"
+    
 
-HelloWorld("print")
+if __name__ == "__main__":
+    car = Car()
+    print(car)
+    car.fill_up()
+    print(car)
+    car.drive(20)
+    print(car)
+    car.drive(50)
+    print(car)
+    car.drive(10)
+    print(car)
+    car.fill_up()
+    car.fill_up()
+    print(car)
