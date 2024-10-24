ab994623c2993bd9423fbcc22c138dce688547cb
Pedro Paulo <gsepi.ufba@gmail.com>
2024-10-24 10:04:11 -0300

    resolvido "game_museum"

--- Part-10/part10-02_game_museum/src/game_museum.py
+++ Part-10/part10-02_game_museum/src/game_museum.py
@@ -1,4 +1,3 @@
-# TEE RATKAISUSI TÄHÄN:
 class ComputerGame:
     def __init__(self, name: str, publisher: str, year: int):
         self.name = name
@@ -15,3 +14,25 @@ class GameWarehouse:
     def list_games(self):
         return self.__games
 
+class GameMuseum(GameWarehouse):
+    def __init__(self):
+        super().__init__()
+
+    def list_games(self):
+        games = super().list_games()
+        new_list = []
+
+        for game in games:
+            if game.year < 1990:
+                new_list.append(game)
+
+        return new_list
+    
+
+if __name__ == "__main__":
+    museum = GameMuseum()
+    museum.add_game(ComputerGame("Pacman", "Namco", 1980))
+    museum.add_game(ComputerGame("GTA 2", "Rockstar", 1999))
+    museum.add_game(ComputerGame("Bubble Bobble", "Taito", 1986))
+    for game in museum.list_games():
+        print(game.name)
