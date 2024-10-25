59cd37d582cb459db23f8958e6a9119f687d70a3
Pedro Paulo <gsepi.ufba@gmail.com>
2024-10-25 16:23:26 -0300

    resolvido "word_game"

--- Part-10/part10-04_word_game/src/word_game.py
+++ Part-10/part10-04_word_game/src/word_game.py
@@ -1,4 +1,3 @@
-# Write your solution here
 import random
 
 class WordGame():
@@ -30,4 +29,70 @@ class WordGame():
         print("game over, wins:")
         print(f"player 1: {self.wins1}")
         print(f"player 2: {self.wins2}")
+
+
+class LongestWord(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        if len(player1_word) > len(player2_word):
+            return 1
+        elif len(player2_word) > len(player1_word):
+            return 2
+        
+
+class MostVowels(WordGame):
+    vowels = "aeiou"
+
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+    
+    def round_winner(self, player1_word: str, player2_word: str):
+        player1_vowels = 0
+        player2_vowels = 0
+
+        for letter in player1_word:
+            if letter in MostVowels.vowels:
+                player1_vowels += 1
+
+        for letter in player2_word:
+            if letter in MostVowels.vowels:
+                player2_vowels += 1
+
+        if player1_vowels > player2_vowels:
+            return 1
+        elif player2_vowels > player1_vowels:
+            return 2
+
+
+class RockPaperScissors(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        words = ["rock", "paper", "scissors"]
+        player1_wins = [("rock", "scissors"), ("paper", "rock"), ("scissors", "paper")]
+
+        if player1_word not in words:
+            if player2_word in words:
+                return 2
+            else:
+                return
+        elif player2_word not in words:
+            return 1
+        
+        if player1_word == player2_word:
+            return
+        
+        for play in player1_wins:
+            if (player1_word, player2_word) == play:
+                return 1
+            
+        return 2
+    
+
+if __name__ == "__main__":
+    p = RockPaperScissors(4)
+    p.play()
 
