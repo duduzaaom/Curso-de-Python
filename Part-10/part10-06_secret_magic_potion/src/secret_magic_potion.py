38bd4724d542c7c2d148fd54862b7dd7ec9cf658
Pedro Paulo <gsepi.ufba@gmail.com>
2024-10-30 18:23:42 -0300

    resolvido secret_magic_potion

--- Part-10/part10-06_secret_magic_potion/src/secret_magic_potion.py
+++ Part-10/part10-06_secret_magic_potion/src/secret_magic_potion.py
@@ -1,4 +1,3 @@
-# Write your solution here:
 class MagicPotion:
     def __init__(self, name: str):
         self._name = name
@@ -13,3 +12,35 @@ class MagicPotion:
             print(f"{ingredient[0]} {ingredient[1]} grams")
 
 
+
+class SecretMagicPotion(MagicPotion):
+    def __init__(self, name: str, password: str):
+        super().__init__(name)
+        self._password = password
+
+    
+    def add_ingredient(self, ingredient: str, amount: float, password: str):
+        if password == self._password:
+            super().add_ingredient(ingredient, amount)
+        else:
+            raise ValueError("This is not the correct password!")
+        
+    
+    def print_recipe(self, password: str):
+        if password == self._password:
+            super().print_recipe()
+        else:
+            raise ValueError("This is not the correct password!")
+        
+
+if __name__ == "__main__":
+    diminuendo = SecretMagicPotion("Diminuendo maximus", "hocuspocus")
+    diminuendo.add_ingredient("Toadstool", 1.5, "hocuspocus")
+    diminuendo.add_ingredient("Magic sand", 3.0, "hocuspocus")
+    diminuendo.add_ingredient("Frogspawn", 4.0, "hocuspocus")
+    diminuendo.print_recipe("hocuspocus")
+
+    diminuendo.print_recipe("pocushocus") # WRONG password!
+
+
+        
