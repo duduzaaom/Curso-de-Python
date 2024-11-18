f62ce03e6a9b74a1dbe55b8b3ac475b32e6d00c5
Eduardo Augusto <eduardoaugusto314@gmail.com>
2024-11-18 17:49:06 -0300

    resolvido lengths_of_strings e most_common_words

--- Part-11/part11-12_most_common_words/src/most_common_words.py
+++ Part-11/part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,21 @@
-# WRITE YOUR SOLUTION HERE:
+def remove_punctuation(word: str):
+    if "." in word or "," in word:
+        return word[:-1]
+    return word
+
+
+def process_file(filename: str):
+    result = []
+    with open(filename) as file:
+        for line in file:
+            result += line.strip().split()
+
+    return [remove_punctuation(word) for word in result]
+
+
+def most_common_words(filename: str, lower_limit: int):
+    pass
+
+
+if __name__ == "__main__":
+    print(process_file("programming.txt"))
