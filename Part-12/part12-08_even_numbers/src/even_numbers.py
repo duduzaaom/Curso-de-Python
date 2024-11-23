a7bdb9189b24d08179d94ee5ee87f2bbe3c6ed93
Eduardo Augusto <eduardoaugusto314@gmail.com>
2024-11-23 10:02:19 -0300

    resolvido even_numbers

--- Part-12/part12-08_even_numbers/src/even_numbers.py
+++ Part-12/part12-08_even_numbers/src/even_numbers.py
@@ -1,1 +1,11 @@
-# Write your solution here
+def even_numbers(beginning: int, maximum: int):
+    for number in range(beginning, maximum + 1):
+        if number % 2 == 0:
+            yield number
+
+
+if __name__ == "__main__":
+    numbers = even_numbers(11, 21)
+    for number in numbers:
+        print(number)
+    
