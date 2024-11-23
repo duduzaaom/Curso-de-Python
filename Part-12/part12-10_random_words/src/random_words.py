fcbfa558a457627dc8c40fab70933bc665d61aa4
Eduardo Augusto <eduardoaugusto314@gmail.com>
2024-11-23 10:31:36 -0300

    resolvido random_words

--- Part-12/part12-10_random_words/src/random_words.py
+++ Part-12/part12-10_random_words/src/random_words.py
@@ -1,1 +1,18 @@
-# Write your solution here:
+import random
+
+def random_words(characters: str, length: int):
+        word = ""
+        for i in range(length):
+            word += random.choice(characters)
+
+        return word
+
+
+def word_generator(characters: str, length: int, amount: int):
+    return (random_words(characters, length) for i in range(amount))
+
+
+if __name__ == "__main__":
+    wordgen = word_generator("abcdefg", 3, 5)
+    for word in wordgen:
+        print(word)
